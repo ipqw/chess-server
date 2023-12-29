6d9a28f61e3273ebd25d29a38d9313a7a80007a4
ipqw <iga538741@gmail.com>
2023-12-29 22:53:00 +0400

    update .env 4

--- src/db.ts
+++ src/db.ts
@@ -1,13 +1,14 @@
 import { Sequelize } from 'sequelize'
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME || 'chess',
-    process.env.DB_USER || 'postgres',
-    process.env.DB_PASSWORD,
-    {
-        dialect: 'postgres',
-        host: process.env.DB_HOST,
-        port: Number(process.env.DB_PORT),
-    }
-)
+// const sequelize = new Sequelize(
+//     process.env.DB_NAME || 'chess',
+//     process.env.DB_USER || 'postgres',
+//     process.env.DB_PASSWORD,
+//     {
+//         dialect: 'postgres',
+//         host: process.env.DB_HOST,
+//         port: Number(process.env.DB_PORT),
+//     }
+// )
+const sequelize = new Sequelize('postgres://bpglqpfg:1YTzhh6LudujpW1xSICDJi_B98Hn2x8_@mel.db.elephantsql.com/bpglqpfg')
 export default sequelize
