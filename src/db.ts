6399f61e0f9c8b43a7aa24531d6ba4b6c2a50d3b
ipqw <iga538741@gmail.com>
2024-01-03 15:40:25 +0400

    zxbzxb

--- src/db.ts
+++ src/db.ts
@@ -2,7 +2,6 @@ import { Sequelize } from 'sequelize'
 
 const sequelize = new Sequelize('postgres://bpglqpfg:1YTzhh6LudujpW1xSICDJi_B98Hn2x8_@mel.db.elephantsql.com/bpglqpfg',
     {
-        dialect: 'postgres',
         dialectModule: require('pg'),
         host: process.env.DB_HOST,
         port: Number(process.env.DB_PORT),
