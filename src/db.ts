c139641828d373fa7623d42e3add4534b59c8261
ipqw <iga538741@gmail.com>
2025-02-08 19:53:08 +0400

    fix

--- src/db.ts
+++ src/db.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize'
 import pg from 'pg';
 
-const sequelize = new Sequelize('postgres://bpglqpfg:1YTzhh6LudujpW1xSICDJi_B98Hn2x8_@mel.db.elephantsql.com/bpglqpfg',
+const sequelize = new Sequelize('postgres://neondb_owner:npg_JEmGs8QgLup7@ep-misty-cloud-a2vgrmav-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require',
     {
         dialect: 'postgres',
         dialectModule: pg,
